a2e051e813837726a6fc0cfb4978ad3275510c69
Tamir <79493337+Tungalagtamir@users.noreply.github.com>
2025-11-17 12:00:00 +0000

    Initial commit

---
+++ src/ca_2/MainOrganization.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ca_2;
+
+/**
+ *
+ * @author tamir
+ */
+public class MainOrganization {
+
+     public static void main(String[] args) {
+        System.out.println("===== Organization system =======");
+     }
+}
+
